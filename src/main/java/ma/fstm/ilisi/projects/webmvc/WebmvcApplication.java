993b608718ff6b1a105f8070df359c4c5c92f70a
elbeggar <elbeggar@ElbeggarHost>
2024-02-27 19:21:37 +0100

    Second Commit

--- src/main/java/ma/fstm/ilisi/projects/webmvc/WebmvcApplication.java
+++ src/main/java/ma/fstm/ilisi/projects/webmvc/WebmvcApplication.java
@@ -1,13 +1,20 @@
 package ma.fstm.ilisi.projects.webmvc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebmvcApplication {
+public class WebmvcApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebmvcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
